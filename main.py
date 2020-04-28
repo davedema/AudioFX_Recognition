4b5c56f4247457b44d231aaeee478727f00569f9
Waddafunk <jacopo.piccirillo@gmail.com>
2020-04-29 01:49:50 +0200

    fix error
    code running

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ dict_test_features = testloop.getdicttestfeatures()  # compute test features
 #dict_train_features = dataloader.Trainselected()
 # TODO: Qui dobbiamo selezionare le colonne delle matrici
 X_test = [dict_test_features[c] for c in glob.classes()] # same as X_train
-y_test = [np.ones(X_train[0].shape[0],) * i for i in np.arange(3)]  # dictionaries
+y_test = [np.ones(X_test[0].shape[0],) * i for i in np.arange(3)]  # dictionaries
 y_test_mc = np.concatenate((y_test[0], y_test[1], y_test[2]), axis=0)
 # X_test_normalized = [((X_test[c] - dataloader.featmin()) / (dataloader.featmax() - dataloader.featmin())) for c in np.arange(len(glob.classes()))]  # normalized matrix
 X_test_normalized = [((X_test[c] - feat_min) / (feat_max - feat_min))
