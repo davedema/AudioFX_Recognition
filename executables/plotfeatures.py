a4862277e3ee2829aee2e6521f9127e58e18e994
David-Badiane <55662004+David-Badiane@users.noreply.github.com>
2020-05-02 23:47:21 +0200

    Update plotfeatures.py

--- executables/plotfeatures.py
+++ executables/plotfeatures.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from executables.modules.analysislab.user_interface import featuresnames, classes
 from numpy import arange
+import seaborn as sns
 
 
 # param feat feature array, param i column of feature, optional
@@ -32,6 +33,21 @@ def plotfeats_per_class(dict_train_features):
 
 # one plot per feature
 def plotfeats(dict_train_features):
+
+    for feat_index, feat in enumerate(featuresnames()):
+
+        train_features_0 = dict_train_features[0]
+        train_features_1 = dict_train_features[1]
+        train_features_2 = dict_train_features[2]
+
+        plt.figure(figsize=(10, 10))
+        for i in arange(len(classes())):
+            sns.distplot(dict_train_features[i][:, feat_index], label='Histogram for {} of feature {}'.format(classes()[i],feat));
+
+        plt.legend()
+        plt.grid(True)
+
+
     for c in arange(len(featuresnames())):
         plt.figure(figsize=(10, 10))
         for i in arange(len(classes())):
@@ -42,7 +58,10 @@ def plotfeats(dict_train_features):
                 plt.title(featuresnames()[c] + ' coefficients')
             plt.ylabel(classes()[i])
             plt.grid(True)
-        plt.show()
+    plt.show()
+
+
+
 
 
 if __name__ == "__main__":  # executable from terminal
