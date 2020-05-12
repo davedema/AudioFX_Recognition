f34d21cd8fdc2acda6b1d6b29a9e49ac62e6ab2e
David-Badiane <55662004+David-Badiane@users.noreply.github.com>
2020-05-13 00:39:06 +0200

    Add files via upload

---
+++ executables/main.py
@@ -0,0 +1,9 @@
+from executables import maintest, maintrain, main_traintest
+from executables.modules.analysislab import user_interface
+if user_interface.generate_datasets():
+    main_traintest.traintest()
+    maintrain.train()  # train
+    maintest.test()  # test
+else:
+    maintrain.train()  # train
+    maintest.test()  # test
