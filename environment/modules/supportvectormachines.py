08301653f1616c16ed6706eef5df888fb4b6b900
David-Badiane <55662004+David-Badiane@users.noreply.github.com>
2020-05-17 04:13:45 +0200

    reacts to classes()

--- environment/modules/supportvectormachines.py
+++ environment/modules/supportvectormachines.py
@@ -1,53 +1,65 @@
 import sklearn.svm
 import numpy as np
-from sklearn.model_selection import cross_val_score
-from environment.modules.analysislab.user_interface import kfold
+import itertools
 
+from sklearn.model_selection import cross_val_score
+from environment.modules.analysislab.user_interface import kfold,classes
 
 def getpredictions(X_train_normalized, y_train, X_test_mc_normalized):
-    SVM_parameters = {
-        'C': 1,
-        'kernel': 'rbf',
-    }
-    clf_01 = sklearn.svm.SVC(**SVM_parameters, probability=True)
-    clf_02 = sklearn.svm.SVC(**SVM_parameters, probability=True)
-    clf_12 = sklearn.svm.SVC(**SVM_parameters, probability=True)
 
-    X_01 = np.concatenate((X_train_normalized[0], X_train_normalized[1]), axis=0)
-    y_01 = np.concatenate((y_train[0], y_train[1]), axis=0)
-    X_02 = np.concatenate((X_train_normalized[0], X_train_normalized[2]), axis=0)
-    y_02 = np.concatenate((y_train[0], y_train[2]), axis=0)
-    X_12 = np.concatenate((X_train_normalized[1], X_train_normalized[2]), axis=0)
-    y_12 = np.concatenate((y_train[1], y_train[2]), axis=0)
-    clf_01.fit(X_01, y_01)
-    clf_02.fit(X_02, y_02)
-    clf_12.fit(X_12, y_12)
-    print("\nCross validated scores, k = 5: \n")
-    scores = ['accuracy', 'precision_micro', 'recall_macro', 'f1_macro', 'roc_auc']
+    scores = ['accuracy','precision_micro','recall_macro','f1_macro','roc_auc']
     overall_scores = {'accuracy': [], 'precision_micro': [], 'recall_macro': [], 'f1_macro': [], 'roc_auc': []}
-    for s in scores:
-        score1 = cross_val_score(clf_01, X_01, y_01, cv=kfold(), scoring=s)
-        score2 = cross_val_score(clf_02, X_02, y_02, cv=kfold(), scoring=s)
-        score3 = cross_val_score(clf_12, X_12, y_12, cv=kfold(), scoring=s)
-        score0 = np.concatenate((score1, score2, score3), axis=0)
-        score = np.average(score0)
-        print('\n')
-        print(s)
-        print('NoFX/Distortion')
-        print(score1)
-        print('NoFX/Tremolo')
-        print(score2)
-        print('Distortion/Tremolo')
-        print(score3)
 
-        overall_scores[s] = score
+    couples = list(itertools.combinations(classes(), 2))
+    num_couples = len(couples)
+    num_test_files = X_test_mc_normalized.shape[0]
+
+    scores_per_couple = np.zeros((num_couples, len(scores)))
+    y_test_predicted_mc = []
+
+    for n in np.arange(num_couples):
+        class_0 = couples[n][0]
+        index_0 = classes().index(class_0)
+        class_1 = couples[n][1]
+        index_1 = classes().index(class_1)
+
+        SVM_parameters = {
+            'C': 1,
+            'kernel': 'rbf',
+        };
+
+        clf = sklearn.svm.SVC(**SVM_parameters, probability=True)
+        X_train_0 = X_train_normalized[index_0]
+        X_train_1 = X_train_normalized[index_1]
+        y_train_0 = y_train[index_0]
+        y_train_1 = y_train[index_1]
+        y = np.concatenate((y_train_0, y_train_1))
+        X = np.concatenate((X_train_0, X_train_1), axis=0)
+
+        clf.fit(X, y)
+        print(class_0,"/",class_1,"\nCross validated scores for kfolds = ", kfold(), ": \n")
+
+        for s in scores:
+            score_array = np.around(cross_val_score(clf, X, y, cv=kfold(), scoring=s), decimals=4) *100
+            score = np.around(np.average(score_array), decimals=2 )
+            print(s, score_array,"average:", score)
+            scores_per_couple[n, scores.index(s)] = score
+        print("\n")
+        y_predicted = clf.predict(X_test_mc_normalized).reshape(-1, 1)
+        y_test_predicted_mc = np.append(y_test_predicted_mc, y_predicted)
+
+    for s in scores:
+        final_score = np.average(scores_per_couple.transpose()[scores.index(s)])
+        overall_scores[s] = np.around(final_score, decimals = 2)
+
+    print("average of crossvalidated scores considering all binary cases")
     print(overall_scores)
-    y_test_predicted_01 = clf_01.predict(X_test_mc_normalized).reshape(-1, 1)
-    y_test_predicted_02 = clf_02.predict(X_test_mc_normalized).reshape(-1, 1)
-    y_test_predicted_12 = clf_12.predict(X_test_mc_normalized).reshape(-1, 1)
-    y_test_predicted_mc = np.concatenate((y_test_predicted_01, y_test_predicted_02, y_test_predicted_12), axis=1)
+
+    y_test_predicted_mc = y_test_predicted_mc.reshape(num_test_files,num_couples)
     y_test_predicted_mc = np.array(y_test_predicted_mc, dtype=np.int)
     y_test_predicted_mv = np.zeros((y_test_predicted_mc.shape[0],))
+
     for i, e in enumerate(y_test_predicted_mc):
         y_test_predicted_mv[i] = np.bincount(e).argmax()
+
     return y_test_predicted_mv
